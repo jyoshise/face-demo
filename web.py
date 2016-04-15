b5b173d7f26f23e5a809ee66a383f88bbdf1b0fc
jyoshise <yoshise@gmail.com>
2016-04-16 02:54:15 +0900

    test3

--- web.py
+++ web.py
@@ -1,8 +1,13 @@
 import os
 import simplejson
 from flask import Flask, render_template, redirect, url_for, request, make_response
+from flask import send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/var/www/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
@@ -24,17 +29,31 @@ class Bar(db.Model):
 def index():
     return render_template('index.html')
 
-@app.route("/upload", methods=["POST"])
-def upload():
-    f = request.files["file"]
-    filename = secure_filename(f.filename)
-    f.save('/var/www/uploads/' + filename))
-    return redirect(url_for("view_upload", filename=filename))
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
-@app.route("/view_upload/<path:filename>")
-def view_upload(filename):
-    return send_from_directory('/var/www/uploads/', filename)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route("/addBar", methods=['POST'])
 def addBar():
