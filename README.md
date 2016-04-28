c99028a4269f805fbdc743ba2cf66a0c335fa7c8
jyoshise <yoshise@gmail.com>
2016-04-28 14:50:43 +0900

    heaven only

--- README.md
+++ README.md
@@ -4,14 +4,12 @@ Written in Python with Flask flamework
 
 Runs on stackato
 
-Powerd by Haven OnDemand API and Google Cloud Vision API
+Powerd by Haven OnDemand API
 
 In order to use:
 
-- Put your Google Vision API credential JSON file
 - set environment variables in manifest.yml:
+
   ```
-  GOOGLE_APPLICATION_CREDENTIALS: <Your Google API credential json file>
   HAVEN_API_KEY: <Your Haven OnDemand API key>
   ```
-  
--- web.py
+++ web.py
@@ -10,8 +10,8 @@ from flask import send_from_directory
 from werkzeug import secure_filename
 from PIL import Image
 from PIL import ImageDraw, ImageFont
-from googleapiclient import discovery
-from oauth2client.client import GoogleCredentials
+# from googleapiclient import discovery
+# from oauth2client.client import GoogleCredentials
 from havenondemand.hodindex import HODClient
 
 app = Flask(__name__)
@@ -27,11 +27,11 @@ def allowed_file(filename):
 API call for Google Cloud Vision API
 credentials must be specified as environment variable GOOGLE_APPLICATION_CREDENTIALS.
 """
-DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
-def get_vision_service():
-    credentials = GoogleCredentials.get_application_default()
-    return discovery.build('vision', 'v1', credentials=credentials,
-                           discoveryServiceUrl=DISCOVERY_URL)
+# DISCOVERY_URL='https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+# def get_vision_service():
+#    credentials = GoogleCredentials.get_application_default()
+#    return discovery.build('vision', 'v1', credentials=credentials,
+#                           discoveryServiceUrl=DISCOVERY_URL)
 
 """
 API call for HPE Heaven OnDemand
@@ -56,25 +56,26 @@ def upload_file():
         max_results = request.form['faces']
         if file and allowed_file(file.filename):
             infile = secure_filename(file.filename)
-            g_outfile = "g_out_" + secure_filename(file.filename)
+#            g_outfile = "g_out_" + secure_filename(file.filename)
             h_outfile = "h_out_" + secure_filename(file.filename)
             input_filename = app.config['UPLOAD_FOLDER'] + "/" + infile
-            g_output_filename = app.config['UPLOAD_FOLDER'] + "/" + g_outfile
+#            g_output_filename = app.config['UPLOAD_FOLDER'] + "/" + g_outfile
             h_output_filename = app.config['UPLOAD_FOLDER'] + "/" + h_outfile
             file.save(input_filename)
 
     with open(input_filename, 'rb') as image:
-        g_faces = detect_face(image, max_results)
-        image.seek(0)
+#        g_faces = detect_face(image, max_results)
+#        image.seek(0)
         h_faces = hodpostrequests('detectfaces', files={'file': image}, data={'additional': True})
         image.seek(0)
-        g_highlight_faces(image, g_faces, g_output_filename)
-        image.seek(0)
+#        g_highlight_faces(image, g_faces, g_output_filename)
+#        image.seek(0)
         h_highlight_faces(image, h_faces, h_output_filename)
 
-    return render_template('show_result.html', input_filename=infile, g_output_filename=g_outfile, h_output_filename=h_outfile, g_count=len(g_faces), h_count=len(h_faces['face']), g_faces=g_faces, h_faces=h_faces)
+#    return render_template('show_result.html', input_filename=infile, g_output_filename=g_outfile, h_output_filename=h_outfile, g_count=len(g_faces), h_count=len(h_faces['face']), g_faces=g_faces, h_faces=h_faces)
+    return render_template('show_result.html', input_filename=infile, h_output_filename=h_outfile, h_count=len(h_faces['face']), h_faces=h_faces)
 
-
+"""
 # [START detect_face]
 def detect_face(face_file, max_results=4):
     image_content = face_file.read()
@@ -96,8 +97,9 @@ def detect_face(face_file, max_results=4):
 
     return response['responses'][0]['faceAnnotations']
 # [END detect_face]
+"""
 
-
+"""
 # [START highlight_faces]
 def g_highlight_faces(image, faces, output_filename):
     im = Image.open(image)
@@ -109,6 +111,7 @@ def g_highlight_faces(image, faces, output_filename):
     del draw
     return im.save(output_filename)
 # [END highlight_faces]
+"""
 
 # [START highlight_faces]
 def h_highlight_faces(image, faces, output_filename):
@@ -126,7 +129,7 @@ def h_highlight_faces(image, faces, output_filename):
         else:
             age = addinfo.get('age')
         draw.line(((left,top),(left+width,top),(left+width,top+height),(left,top+height),(left,top)), width=5, fill='#01a982')
-        draw.text((left,top+height), age, font=fnt, fill='#FFFFFF')
+        draw.text((left,top+height), age, font=fnt, fill='#01a982')
     del draw
     return im.save(output_filename)
 # [END highlight_faces]
