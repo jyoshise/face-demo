3e56f36e05db6ff3143230c23031dcee17f0fd01
jyoshise <yoshise@gmail.com>
2016-06-29 08:25:17 +0900

    test

--- index.py
+++ index.py
@@ -93,11 +93,11 @@ def h_highlight_faces(image, result, output_filename):
 # Draw landmarks
         faceLandmarks = currFace['faceLandmarks']
         for _, currLandmark in faceLandmarks.iteritems():
-            draw.ellipse(((int(currLandmark['x']-1),int(currLandmark['y']-1)),(int(currLandmark['x']+1),int(currLandmark['y']+1))), fill=fillcolor)
+#            draw.ellipse(((int(currLandmark['x']-1),int(currLandmark['y']-1)),(int(currLandmark['x']+1),int(currLandmark['y']+1))), fill=fillcolor)
 
 # Draw text label
-        draw.rectangle(((left,top+height),(left+width,top+height+30)), fill=fillcolor)
-        draw.text((left,top+height), textToWrite, font=fnt, fill='#ffffff')
+#        draw.rectangle(((left,top+height),(left+width,top+height+30)), fill=fillcolor)
+#        draw.text((left,top+height), textToWrite, font=fnt, fill='#ffffff')
 
     del draw
     return im.save(output_filename)
