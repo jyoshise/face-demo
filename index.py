87f3ab45ff3621977fca915871bb9ada23207416
hirotaka-noguchi <hirotaka-noguchi@mail.nissan.co.jp>
2016-10-04 12:59:53 +0900

    Update index.py

--- index.py
+++ index.py
@@ -86,7 +86,7 @@ def h_highlight_faces(image, result, output_filename):
         width = faceRectangle['width']
         height = faceRectangle['height']
         age = faceAttributes['age']
-        age = age-5 if faceAttributes['gender']=='female' else age:
+        age = age-5 if faceAttributes['gender']=='female'
 
         textToWrite = "%c (%d)" % ( 'M' if faceAttributes['gender']=='male' else 'F', age )
         fillcolor = '#55ff00' if faceAttributes['gender']=="male" else '#ff4400'
